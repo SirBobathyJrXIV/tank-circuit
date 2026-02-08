52c394f67a3fba43f979c973f161c2d7b95b3187
SirBobathyJrXIV <phamwarren@gmail.com>
2026-02-08 10:21:19 -0500

    Update test-circuit-reciever1.ino

--- test-circuit1/test-circuit-reciever1/test-circuit-reciever1.ino
+++ test-circuit1/test-circuit-reciever1/test-circuit-reciever1.ino
@@ -16,7 +16,7 @@
 //*/
 // Set the MAC address of the device that will receive the data
 // For example: F4:12:FA:40:64:4C
-const MacAddress peer_mac({0x02, 0x3D, 0xAE, 0x60, 0xCC, 0x78}); //ec:da:3b:bf:ed:10 Pins C3
+const MacAddress peer_mac({0x98, 0x3D, 0xAE, 0x60, 0xCC, 0x78}); //ec:da:3b:bf:ed:10 Pins C3
 
 ESP_NOW_Serial_Class NowSerial(peer_mac, ESPNOW_WIFI_CHANNEL, ESPNOW_WIFI_IF);
 
@@ -28,6 +28,18 @@ struct data_packet
 };
 
 data_packet incoming_data { 0, 0, 0 };
+
+void readMacAddress(){
+  uint8_t baseMac[6];
+  esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
+  if (ret == ESP_OK) {
+    Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
+                  baseMac[0], baseMac[1], baseMac[2],
+                  baseMac[3], baseMac[4], baseMac[5]);
+  } else {
+    Serial.println("Failed to read MAC address");
+  }
+}
 
 void setup()
 {
@@ -41,6 +53,7 @@ void setup()
   }
 
   NowSerial.begin(115200);
+  readMacAddress();
 }
 
 void loop()
